a4762142df33b3cb62d17d4ed3b5cfe866473591
issakhaly <157059290+issakhaly@users.noreply.github.com>
2024-10-07 21:08:59 +0000

    Update config.js

--- config.js
+++ config.js
@@ -5,6 +5,6 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "",
+SESSION_ID: process.env.SESSION_ID || "𝙰𝚂𝙸𝚃𝙷𝙰-𝙼𝙳=G6AmxKAC#RNRnn6uOtlqT1JJKtPJBKKBboMzNCr_XME64Dn_Bfcs",
 MONGODB: process.env.MONGODB || "",//enter mongo db url
 };
